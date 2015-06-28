50c1073a46d638dac3cdb2185503d1a430bf7333
Steve Bertolani <sjbertolani@ucdavis.edu>
2015-06-28 15:16:31 -0700

    Adding constant_seed for reproducibility

--- enzdes/run_design.sh
+++ enzdes/run_design.sh
@@ -16,4 +16,4 @@ name=$(echo $infile|cut -d . -f1)
 mkdir $name
 cd $name
 
-$ROSETTA_BIN/rosetta_scripts.default.linuxgccrelease @$working_dir/enzflags_parser -overwrite -parser:protocol $working_dir/design_on.xml -s /home/bertolan/work/bookmatcher/matcher/$infile -nstruct 10 -database $ROSETTA_DB -score:weights $ROSETTA_DB/scoring/weights/talaris2013_cst.wts
+$ROSETTA_BIN/rosetta_scripts.default.linuxgccrelease @$working_dir/enzflags_parser -overwrite -parser:protocol $working_dir/design_on.xml -s /home/bertolan/work/bookmatcher/matcher/$infile -nstruct 10 -database $ROSETTA_DB -score:weights $ROSETTA_DB/scoring/weights/talaris2013_cst.wts -constant_seed T
