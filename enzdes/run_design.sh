817080b825d7c8c6bc9d23f4f235629d0c2ffc0f
Steve Bertolani <sjbertolani@ucdavis.edu>
2015-06-29 15:52:26 -0700

    Update run_design.sh

--- enzdes/run_design.sh
+++ enzdes/run_design.sh
@@ -10,10 +10,10 @@
 #$ -v ROSETTA_DB
 
 working_dir=$(pwd)
-infile=`awk 'NR==n' n=$SGE_TASK_ID /home/bertolan/work/bookmatcher/mymatches.list`
+infile=`awk 'NR==n' n=$SGE_TASK_ID mymatches.list`
 name=$(echo $infile|cut -d . -f1)
 
 mkdir $name
 cd $name
 
-$ROSETTA_BIN/rosetta_scripts.default.linuxgccrelease @$working_dir/enzflags_parser -overwrite -parser:protocol $working_dir/design_on.xml -s /home/bertolan/work/bookmatcher/matcher/$infile -nstruct 10 -database $ROSETTA_DB -score:weights $ROSETTA_DB/scoring/weights/talaris2013_cst.wts
+$ROSETTA_BIN/rosetta_scripts.default.linuxgccrelease @$working_dir/enzflags_parser -overwrite -parser:protocol $working_dir/design_on.xml -s $working_dir/../matcher/$infile -nstruct 10 -database $ROSETTA_DB -score:weights $ROSETTA_DB/scoring/weights/talaris2013_cst.wts -extra_res_fa $working_dir/../matcher/LG1.params -enzdes::cstfile $working_dir/../matcher/LG1.enzdes.cst 
